7cb07571e6fc323c89c5d7a6a45ad94f7bb3ef3d
SOFe <sofe2038@gmail.com>
2020-09-22 19:12:53 +0800

    Initial commit

---
+++ test-bin/src/main.rs
@@ -0,0 +1,9 @@
+#[test_macro::derive_default_component]
+#[allow(unused_imports)]
+struct Foo;
+
+#[test_macro::derive_default_component]
+#[allow(unused_imports)]
+struct Bar;
+
+fn main() {}
---
+++ test-macro/src/lib.rs
@@ -0,0 +1,6 @@
+qualify_derive::declare! {
+    derive_default_component derives ::specs::Component;
+    use specs::Component;
+    use specs::storage::DenseVecStorage;
+    attr storage
+}
